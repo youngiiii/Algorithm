10b53f0b4b5924d436262639c8ec56f328ecd4b4
Ahyoung <seo.ox3@gmail.com>
2020-05-21 20:58:57 +0900

    C# 특수문자 print test

---
+++ test04/test04/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //\    /\
+            // )  ( ')
+            //(  /  )
+            // \(__)|
+
+            //위의 그림을 출력하시오
+            //C언어를 배우고 싶다는 친구에게 문제를 냈는데 특수문자 출력관련해서 test해봄
+
+            Console.WriteLine("\\    /\\");
+            Console.WriteLine(" )  ( ')");
+            Console.WriteLine("(  /  )");
+            Console.WriteLine(" \\(__)|");
+            Console.ReadLine();
+        }
+    }
+}
