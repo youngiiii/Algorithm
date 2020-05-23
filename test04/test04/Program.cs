d6f1684617b6819ba7fedf902c6b0796cd7fa440
Ahyoung <seo.ox3@gmail.com>
2020-05-23 16:47:55 +0900

    Revert "Revert "A+B 빠른 출력(StringBuilder 이용)""
    This reverts commit 2a7380502f35cfecb40769453d1c6e5b4a56d1bb.

--- test04/test04/Program.cs
+++ test04/test04/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Resources;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,19 +13,18 @@ namespace test04
     {
         static void Main(string[] args)
         {
+            //StreamReader로 읽고, StringBuilder로 출력을 모아 놓았다가 그 String을 Console.WriteLine하는 방법이 있습니다. 
+            //BufferedStream과 StringWriter로 조금 더 향상시킬 수 있는 것 같으나 자세한 것은 다른 분의 답변을 기다리겠습니다.
 
-            //\    /\
-            // )  ( ')
-            //(  /  )
-            // \(__)|
-
-            //위의 그림을 출력하시오
-            //C언어를 배우고 싶다는 친구에게 문제를 냈는데 특수문자 출력관련해서 test해봄
-
-            Console.WriteLine("\\    /\\");
-            Console.WriteLine(" )  ( ')");
-            Console.WriteLine("(  /  )");
-            Console.WriteLine(" \\(__)|");
+            int cnt = int.Parse(Console.ReadLine());
+            
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < cnt; i++)
+            {
+                string[] str = Console.ReadLine().Split(' ');
+                builder.Append((int.Parse(str[0]) + int.Parse(str[1])).ToString() + "\n");
+            }
+            Console.WriteLine(builder.ToString());
             Console.ReadLine();
         }
     }
