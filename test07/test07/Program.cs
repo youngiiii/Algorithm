a1481ddeb753a419ecae08df491c7330a54b0b3e
Ahyoung <seo.ox3@gmail.com>
2020-05-24 20:12:50 +0900

    A+B 아름답게 출력하는법

--- test07/test07/Program.cs
+++ test07/test07/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace test07
 {
@@ -7,7 +9,34 @@ namespace test07
     {
         static void Main(string[] args)
         {
-            
+            //두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+            int num = int.Parse(Console.ReadLine());
+
+            var lst = new List<string>();
+            for (int i = 0; i < num; i++)
+            {
+                lst.Add(Console.ReadLine());
+            }
+
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < num; i++)
+            {
+                string aa = "Case #" + (i + 1) + ": ";
+
+                string[] str = lst[i].Split(' ');
+                builder.Append(aa + (int.Parse(str[0]) + int.Parse(str[1])).ToString() + "\n");
+            }
+            Console.WriteLine(builder.ToString());
+
+            builder = new StringBuilder();
+            for (int i = 0; i < num; i++)
+            {
+                string[] str = lst[i].Split(' ');
+                string aa = "Case #" + (i + 1) + ": " + str[0] + " + " + str[1] + " = ";
+                builder.Append(aa + (int.Parse(str[0]) + int.Parse(str[1])).ToString() + "\n");
+            }
+            Console.WriteLine(builder.ToString());
+
         }
     }
 }
