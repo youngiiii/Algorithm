01cd8ffaf116f8412a3db21f7dfdf8cfd038b781
Ahyoung <seo.ox3@gmail.com>
2020-05-25 17:27:14 +0900

    별출력

---
+++ test08/test08/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //입력 : 5
+            //출력
+            //*
+            //**
+            //***
+            //****
+            //*****
+            int num = int.Parse(Console.ReadLine());
+
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < num; i++)
+            {
+                for (int j = 0; j < (i + 1); j++)
+                {
+                    builder.Append("*");
+                }
+                builder.Append("\n");
+            }
+            Console.WriteLine(builder.ToString());
+
+            //    *
+            //   **
+            //  ***
+            // ****
+            //*****
+            //builder = new StringBuilder();
+            //for (int i = 0; i < num; i++)
+            //{
+            //    for (int j = i; j < num - i; j++)
+            //    {
+            //        builder.Append(" ");
+            //    }
+
+            //    for (int j = 0; j < (i + 1); j++)
+            //    {
+            //        builder.Append("*");
+            //    }
+            //    builder.Append("\n");
+            //}
+            //Console.WriteLine(builder.ToString());
+            //Console.ReadLine();
+        }
+    }
+}
