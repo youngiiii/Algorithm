08744fe8c26aa04ec24688449daa59285be73d83
Ahyoung <seo.ox3@gmail.com>
2020-05-25 17:52:34 +0900

    x를 입력받아 x보다 작은 수를 출력한다.

---
+++ test09/test09/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // X보다 작은 수를 입력받은 순서대로 공백으로 구분해 출력한다. 
+            // X보다 작은 수는 적어도 하나 존재한다.
+            string num = Console.ReadLine();
+            string[] n = num.Split(' ');
+
+            string numLst = Console.ReadLine();
+            string[] lst = numLst.Split(' ');
+
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < lst.Count(); i++)
+            {
+                if(int.Parse(lst[i]) < int.Parse(n[1]))
+                {
+                    builder.Append(lst[i] + " ");
+                }
+            }
+            Console.WriteLine(builder.ToString());
+            Console.ReadLine();
+        }
+    }
+}
