ee25ce147ea434831a8e7fd4f49d7d1f226264e0
Ahyoung <seo.ox3@gmail.com>
2020-05-27 21:48:32 +0900

    세 정수 A, B, C가 주어진다. 이때, 두 번째로 큰 정수를 출력하는 프로그램 (While)

--- test13/test13/Program.cs
+++ test13/test13/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace test13
 {
     class Program
@@ -6,21 +8,35 @@ namespace test13
         static void Main(string[] args)
         {
             //세 정수 A, B, C가 주어진다. 이때, 두 번째로 큰 정수를 출력하는 프로그램
-            var numLst = new int[3];
-            for (int i = 0; i < 3; i++)
+            //걍 푼거
+            //var numLst = new int[3];
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    numLst[i] = int.Parse(Console.ReadLine());
+            //    for (int j = 0; j < (i + 1); j++)
+            //    {
+            //        if (numLst[j] < numLst[i])
+            //        {
+            //            int temp = numLst[i];
+            //            numLst[i] = numLst[j];
+            //            numLst[j] = temp;
+            //        }
+            //    }
+            //}
+            //Console.WriteLine(numLst[1]);
+            
+            //Sort이용
+            string[] s = Console.ReadLine().Split();
+            List<int> list = new List<int>();
+
+            foreach (var item in s)
             {
-                numLst[i] = int.Parse(Console.ReadLine());
-                for (int j = 0; j < (i + 1); j++)
-                {
-                    if (numLst[j] < numLst[i])
-                    {
-                        int temp = numLst[i];
-                        numLst[i] = numLst[j];
-                        numLst[j] = temp;
-                    }
-                }
+                list.Add(int.Parse(item));
             }
-            Console.WriteLine(numLst[1]);
+            list.Sort(delegate (int x, int y) {
+                return (x > y) ? 1 : (x < y) ? -1 : 0;
+            });
+            Console.WriteLine(list[1]);
             Console.ReadLine();
         }
     }
