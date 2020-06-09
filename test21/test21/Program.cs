0674a3f9cd9c9f3e5ceef909b45ec9a8bf9f0853
Ahyoung <seo.ox3@gmail.com>
2020-06-09 15:07:38 +0900

    아스키 코드 출력, A + B 출력

--- test21/test21/Program.cs
+++ test21/test21/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace test21
 {
@@ -6,9 +7,21 @@ namespace test21
     {
         static void Main(string[] args)
         {
-            var b = Convert.ToChar(Console.ReadLine()); // char
-            int ascii = Convert.ToInt32(b); // Ascii 
-            Console.WriteLine(ascii);
+            //아스키 코드 출력
+            //var b = Convert.ToChar(Console.ReadLine()); // char
+            //int ascii = Convert.ToInt32(b); // Ascii 
+            //Console.WriteLine(ascii);
+
+            //A + B 출력
+            int cnt = int.Parse(Console.ReadLine());
+            var b = Console.ReadLine();
+
+            int sum = 0;
+            for (int i = 0; i < cnt; i++)
+            {
+                sum += int.Parse(b[i].ToString());
+            }
+            Console.WriteLine(sum);
         }
     }
 }
