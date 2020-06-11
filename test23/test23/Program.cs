5b42189c080117b5e5f7d39a93fa1de5f4ff8089
Ahyoung <seo.ox3@gmail.com>
2020-06-11 15:03:03 +0900

    문자열 S를 입력받은 후에, 각 문자를 R번 반복해 새 문자열 P를 만든 후 출력

---
+++ test23/test23/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace test23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cnt = int.Parse(Console.ReadLine());
+            string result = "";
+            for (int i = 0; i < cnt; i++)
+            {
+                var input = Console.ReadLine().Split(" ");
+
+                for (int j = 0; j < input[1].Length; j++)
+                {
+                    for (int k = 0; k < int.Parse(input[0]); k++)
+                    {
+                        result += input[1].Substring(j, 1);
+                    }
+                }
+                result += "\n";
+            }
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
